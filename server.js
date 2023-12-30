c20bf33e90f1dec02a05b2aa6b64d72f3b7d86a8
David Adeogun <adeogundavidt@gmail.com>
2023-12-30 13:06:49 -0500

    Add ejs package and update server.js to use environment variable for port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000
 
 // MongoDB Connection
 mongoose.connect(process.env.MongoDB_URI)
